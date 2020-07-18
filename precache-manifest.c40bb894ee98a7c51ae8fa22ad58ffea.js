146541b5ac0a9debf6221bfe65b1b2083c4a0f77
Seyed Farzam Mirmoeini <farzam.mirmoeini@gmail.com>
2020-07-18 23:43:12 +0430

    Updates

---
+++ precache-manifest.c40bb894ee98a7c51ae8fa22ad58ffea.js
@@ -0,0 +1,270 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3f1d3f3936baeb8152e0268da1aa3e6",
+    "url": "/Chitra/index.html"
+  },
+  {
+    "revision": "cdd5a4da64207660858f",
+    "url": "/Chitra/static/css/main.8b26e17c.chunk.css"
+  },
+  {
+    "revision": "54a1ff475ead537e7778",
+    "url": "/Chitra/static/js/2.8c71736e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Chitra/static/js/2.8c71736e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdd5a4da64207660858f",
+    "url": "/Chitra/static/js/main.a1133fbd.chunk.js"
+  },
+  {
+    "revision": "7d3adc14bdba235e6e67",
+    "url": "/Chitra/static/js/runtime-main.ed3e56df.js"
+  },
+  {
+    "revision": "469aa871021eb4024892de9e94a33f2b",
+    "url": "/Chitra/static/media/dana-black.469aa871.woff"
+  },
+  {
+    "revision": "84b0bfd70a626b26a08085d3bf7f4fda",
+    "url": "/Chitra/static/media/dana-black.84b0bfd7.eot"
+  },
+  {
+    "revision": "ca6825ba7188242382da95bfa266dd38",
+    "url": "/Chitra/static/media/dana-black.ca6825ba.woff2"
+  },
+  {
+    "revision": "296bab1c63596243bf155c912726bc60",
+    "url": "/Chitra/static/media/dana-blackitalic.296bab1c.woff2"
+  },
+  {
+    "revision": "8b939c7da46d3bd6e0ec43a663d78b93",
+    "url": "/Chitra/static/media/dana-blackitalic.8b939c7d.woff"
+  },
+  {
+    "revision": "b0982a373baa1706dd1287529e4fee58",
+    "url": "/Chitra/static/media/dana-blackitalic.b0982a37.eot"
+  },
+  {
+    "revision": "0ebab5978f72ecdee06614d8a465cc3f",
+    "url": "/Chitra/static/media/dana-bold.0ebab597.woff2"
+  },
+  {
+    "revision": "30ff58e0ef12599676e89916caf5a1ec",
+    "url": "/Chitra/static/media/dana-bold.30ff58e0.eot"
+  },
+  {
+    "revision": "6a63572445c00da63142c9dba91f3263",
+    "url": "/Chitra/static/media/dana-bold.6a635724.woff"
+  },
+  {
+    "revision": "0d1f0745adfb6d7fffcb27723bed2554",
+    "url": "/Chitra/static/media/dana-bolditalic.0d1f0745.woff"
+  },
+  {
+    "revision": "2b4e003923b1209d50f95604925264b5",
+    "url": "/Chitra/static/media/dana-bolditalic.2b4e0039.eot"
+  },
+  {
+    "revision": "aadb68cf30830dda5bc30c1338597479",
+    "url": "/Chitra/static/media/dana-bolditalic.aadb68cf.woff2"
+  },
+  {
+    "revision": "60127ebee6d546ba2fcf1c06ef92e9ae",
+    "url": "/Chitra/static/media/dana-demibold.60127ebe.woff"
+  },
+  {
+    "revision": "bdf9b1cd3771f4c69ccd345a9db88f44",
+    "url": "/Chitra/static/media/dana-demibold.bdf9b1cd.eot"
+  },
+  {
+    "revision": "fb2c73e8dea5ffc655cfc5851be88b5e",
+    "url": "/Chitra/static/media/dana-demibold.fb2c73e8.woff2"
+  },
+  {
+    "revision": "542f48f9fad64c99b1f5007d57cae1dd",
+    "url": "/Chitra/static/media/dana-demibolditalic.542f48f9.woff"
+  },
+  {
+    "revision": "7b89cfc7c819b523c55125c283d52c99",
+    "url": "/Chitra/static/media/dana-demibolditalic.7b89cfc7.woff2"
+  },
+  {
+    "revision": "f69aab42d3067d952c136ff1b7afc375",
+    "url": "/Chitra/static/media/dana-demibolditalic.f69aab42.eot"
+  },
+  {
+    "revision": "02fbcc6b83dad3f14e14ca94edea21c1",
+    "url": "/Chitra/static/media/dana-extrabold.02fbcc6b.eot"
+  },
+  {
+    "revision": "affdae901f5ff432b7803790ccaffaaf",
+    "url": "/Chitra/static/media/dana-extrabold.affdae90.woff"
+  },
+  {
+    "revision": "c3e5913aaf0e24c8ff5bc3c395d6053c",
+    "url": "/Chitra/static/media/dana-extrabold.c3e5913a.woff2"
+  },
+  {
+    "revision": "01de873d535de5a8232133d83e5ad836",
+    "url": "/Chitra/static/media/dana-extrabolditalic.01de873d.eot"
+  },
+  {
+    "revision": "47fbae2e7f69d9b87357db8f99b78c96",
+    "url": "/Chitra/static/media/dana-extrabolditalic.47fbae2e.woff"
+  },
+  {
+    "revision": "75cfddb5b44b56a8b6f667e2d8cc1a8b",
+    "url": "/Chitra/static/media/dana-extrabolditalic.75cfddb5.woff2"
+  },
+  {
+    "revision": "013d90b9c9650df8a2b5f8cf3dd55b13",
+    "url": "/Chitra/static/media/dana-extralight.013d90b9.woff"
+  },
+  {
+    "revision": "485846926567ae152176911481229534",
+    "url": "/Chitra/static/media/dana-extralight.48584692.eot"
+  },
+  {
+    "revision": "73137901c9c3d44de96d6fbc80429a25",
+    "url": "/Chitra/static/media/dana-extralight.73137901.woff2"
+  },
+  {
+    "revision": "3e60c359ef4dbb41597a16315034cf00",
+    "url": "/Chitra/static/media/dana-extralightitalic.3e60c359.eot"
+  },
+  {
+    "revision": "bd6d8418421bbf3a039266deb6136038",
+    "url": "/Chitra/static/media/dana-extralightitalic.bd6d8418.woff"
+  },
+  {
+    "revision": "bfdc0c25b7009ddfb9f843cf61dda6c0",
+    "url": "/Chitra/static/media/dana-extralightitalic.bfdc0c25.woff2"
+  },
+  {
+    "revision": "7c43221084001bebaba1d4cd5d11370d",
+    "url": "/Chitra/static/media/dana-light.7c432210.woff"
+  },
+  {
+    "revision": "81479e72933e9417f567fdcf8531b093",
+    "url": "/Chitra/static/media/dana-light.81479e72.woff2"
+  },
+  {
+    "revision": "b0461ebbd078c178574c28061690c0ac",
+    "url": "/Chitra/static/media/dana-light.b0461ebb.eot"
+  },
+  {
+    "revision": "1596cd825e87935f0b8cf6e61ba36519",
+    "url": "/Chitra/static/media/dana-lightitalic.1596cd82.woff2"
+  },
+  {
+    "revision": "2ad8ec6c2de257da0a9534040a52970b",
+    "url": "/Chitra/static/media/dana-lightitalic.2ad8ec6c.eot"
+  },
+  {
+    "revision": "b90f053a6f235441d07e1366cd222727",
+    "url": "/Chitra/static/media/dana-lightitalic.b90f053a.woff"
+  },
+  {
+    "revision": "6553d9ec5ed40d3f103b818bf23e7b20",
+    "url": "/Chitra/static/media/dana-medium.6553d9ec.eot"
+  },
+  {
+    "revision": "7e7274c974ac1bdabd744de59f9330d3",
+    "url": "/Chitra/static/media/dana-medium.7e7274c9.woff"
+  },
+  {
+    "revision": "a3dcc293c65e225adbc952809d961f9c",
+    "url": "/Chitra/static/media/dana-medium.a3dcc293.woff2"
+  },
+  {
+    "revision": "36fb76257f4a152709d783ed43568065",
+    "url": "/Chitra/static/media/dana-mediumitalic.36fb7625.woff"
+  },
+  {
+    "revision": "4ef5bfc5d3c8841f24bf83e1cb11cddf",
+    "url": "/Chitra/static/media/dana-mediumitalic.4ef5bfc5.eot"
+  },
+  {
+    "revision": "9e5bb5d765ff6ee8a69779b3516e1938",
+    "url": "/Chitra/static/media/dana-mediumitalic.9e5bb5d7.woff2"
+  },
+  {
+    "revision": "4e5505f9f8e96c47cb5a68eb3cc97abb",
+    "url": "/Chitra/static/media/dana-regular.4e5505f9.woff"
+  },
+  {
+    "revision": "98434684a13717de380563afcbe715d3",
+    "url": "/Chitra/static/media/dana-regular.98434684.eot"
+  },
+  {
+    "revision": "ff38c3e5ce4fad46b1f401672d6bf144",
+    "url": "/Chitra/static/media/dana-regular.ff38c3e5.woff2"
+  },
+  {
+    "revision": "22a5b3af11cda07ca1ff8c16fb605560",
+    "url": "/Chitra/static/media/dana-regularitalic.22a5b3af.eot"
+  },
+  {
+    "revision": "8db1364aaf988a70c217c6a8c80e1f74",
+    "url": "/Chitra/static/media/dana-regularitalic.8db1364a.woff"
+  },
+  {
+    "revision": "b41dff6a1f56ff268b5de8e5b8a4ca34",
+    "url": "/Chitra/static/media/dana-regularitalic.b41dff6a.woff2"
+  },
+  {
+    "revision": "1d4fde53ce46fe99742700d75083ace7",
+    "url": "/Chitra/static/media/dana-thin.1d4fde53.woff2"
+  },
+  {
+    "revision": "795f24d5c6c017bf8c155405475778d9",
+    "url": "/Chitra/static/media/dana-thin.795f24d5.woff"
+  },
+  {
+    "revision": "81affc08d65e32113ded925f3165ca19",
+    "url": "/Chitra/static/media/dana-thin.81affc08.eot"
+  },
+  {
+    "revision": "527537be55e3dc3438dd1ca77c6fc5fd",
+    "url": "/Chitra/static/media/dana-thinitalic.527537be.woff2"
+  },
+  {
+    "revision": "e26594fabc2c497368487276584dddf7",
+    "url": "/Chitra/static/media/dana-thinitalic.e26594fa.woff"
+  },
+  {
+    "revision": "e6aeada19036cfb4b7484af77f3dead5",
+    "url": "/Chitra/static/media/dana-thinitalic.e6aeada1.eot"
+  },
+  {
+    "revision": "41067547e36da7bc0ae5787b3180179a",
+    "url": "/Chitra/static/media/dana-ultrabold.41067547.eot"
+  },
+  {
+    "revision": "709a23d63e044af2752512c79bcaa545",
+    "url": "/Chitra/static/media/dana-ultrabold.709a23d6.woff"
+  },
+  {
+    "revision": "7ca773a5854a301e83d339f7a920b7b8",
+    "url": "/Chitra/static/media/dana-ultrabold.7ca773a5.woff2"
+  },
+  {
+    "revision": "3d34acf55d6289e57c5620256e5e7737",
+    "url": "/Chitra/static/media/dana-ultrabolditalic.3d34acf5.woff2"
+  },
+  {
+    "revision": "afbc6b95d1b3a01d0debda77fd66e6da",
+    "url": "/Chitra/static/media/dana-ultrabolditalic.afbc6b95.eot"
+  },
+  {
+    "revision": "b1751f6a43576098d14a1d5600dd2b02",
+    "url": "/Chitra/static/media/dana-ultrabolditalic.b1751f6a.woff"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Chitra/static/media/logo.5d5d9eef.svg"
+  }
+]);
