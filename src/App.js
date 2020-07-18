83437f6d318fe26cc760317926c5009e81aabd16
Seyed Farzam Mirmoeini <farzam.mirmoeini@gmail.com>
2020-07-18 23:39:59 +0430

    initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './Styles/fonts.css';
+import './Styles/App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={"fullbox bsefid gocenter"}>
+        <div className={"formBox"}>
+            <div className={"container"}>
+                <div className={"row"}>
+                    <div className={"col-sm-6"}>
+                        <h4 className={"title"}>ورود</h4>
+                    </div>
+                    <div className={"col-sm-6"}>
+                        <a className={"sisFormBox"} href={"#"}>
+                           ثبت‌نام
+                            <span></span>
+                        </a>
+                    </div>
+                </div>
+                <div className={"row"}>
+                    <div className={"col-sm-12"}>
+                        <p className={"description"}>
+                            برای ورود به پنل اطلاعات خود را وارد کنید
+                        </p>
+                    </div>
+                </div>
+                <div className={"row"}>
+                    <form>
+                        <div className={"col-sm-12"}>
+                            <label>ایمیل یا نام کاربری</label>
+                            <input type={"text"} placeholder={"nima@gmail.com"} className={"text-left placeholder-text-left"}/>
+                            <label>رمز عبور</label>
+                            <input type={"password"} placeholder={"حداقل ۸ کاراکتر"} className={"text-left"}/>
+                            <label className="checkBoxcontainer">
+                                <input type="checkbox"/>
+                                <span className="checkmark"/>
+                                مرا به خاطر بسپار
+                            </label>
+                            <input type={"submit"} value={"ورود"}/>
+                        </div>
+                    </form>
+                </div>
+                <div className={"row"}>
+                    <div className={"col-sm-12 text-center"}>
+                        <a className={"altFormBox"} href={"#"}>رمز عبورم را فراموش کردم.</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </div>
     </div>
   );
 }
